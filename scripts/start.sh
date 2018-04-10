ef11ec1ebab3c18031ed486186b493895211f811
NickHeap <nickheap@gmail.com>
2018-04-10 09:17:38 +0100

    Shutdown properly

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,18 +6,18 @@ signal_handler() {
 
     echo "Stopping container"
     # stop the webspeed, name server and adminserver
-    #echo "Stopping webspeed wsbroker1"
-    #asbman -stop -name wsbroker1
-    #echo "Stopping name server NS1"
-    #nsman -stop -name NS1
-    #echo "Stopping admin server"
-    #proadsv -stop
+    echo "Stopping webspeed wsbroker1"
+    asbman -stop -name wsbroker1
+    echo "Stopping name server NS1"
+    nsman -stop -name NS1
+    echo "Stopping admin server"
+    proadsv -stop
 
     # graceful shutdown so exit with 0
     exit 0
 }
 # trap SIGTERM and call the handler to cleanup processes
-trap 'kill ${!}; signal_handler' SIGTERM SIGINT
+trap 'signal_handler' SIGTERM SIGINT
 
 # replace values in ubroker.properties file
 sed  "s|3055|${WEBSPEED_PORT}|g;s|wsbroker1|${WEBSPEED_SERVICE}|g;s|3202|${WEBSPEED_MINPORT}|g;s|3502|${WEBSPEED_MAXPORT}|g;s|-p web\/objects\/web-disp.p -weblogerror|${WEBSPEED_STARTUP}|g;s|PROPATH=\${PROPATH}:\${WRKDIR}|PROPATH=${PROPATH}|g;s|5162|${NAMESERVER_PORT}|g;s|srvrLoggingLevel=2|srvrLoggingLevel=${LOGGING_LEVEL}|g;s|srvrLogEntryTypes=DB.Connects|srvrLogEntryTypes=${LOG_ENTRY_TYPES}|g;s|workDir=\$WRKDIR|workDir=\/var\/lib\/openedge\/code\/|g;s|initialSrvrInstance=5|initialSrvrInstance=${WEBSPEED_INITIAL_AGENTS}|g;s|minSrvrInstance=1|minSrvrInstance=${WEBSPEED_MIN_AGENTS}|g;s|maxSrvrInstance=10|maxSrvrInstance=${WEBSPEED_MAX_AGENTS}|g" /usr/dlc/properties/ubroker.properties > /usr/dlc/properties/ubroker.properties.new
