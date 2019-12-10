8173e46d7220df6e36f96351023681ed1b954858
NickHeap <nickheap@gmail.com>
2019-12-10 11:34:16 +0000

    Extra checks for processes

--- scripts/start.sh
+++ scripts/start.sh
@@ -23,14 +23,55 @@ trap 'signal_handler' SIGTERM SIGINT
 sed  "s|3055|${WEBSPEED_PORT}|g;s|wsbroker1|${WEBSPEED_SERVICE}|g;s|3202|${WEBSPEED_MINPORT}|g;s|3502|${WEBSPEED_MAXPORT}|g;s|-p web\/objects\/web-disp.p -weblogerror|${WEBSPEED_STARTUP}|g;s|PROPATH=\${PROPATH}:\${WRKDIR}|PROPATH=${PROPATH}|g;s|5162|${NAMESERVER_PORT}|g;s|srvrLoggingLevel=2|srvrLoggingLevel=${LOGGING_LEVEL}|g;s|srvrLogEntryTypes=DB.Connects|srvrLogEntryTypes=${LOG_ENTRY_TYPES}|g;s|workDir=\$WRKDIR|workDir=\/var\/lib\/openedge\/code\/|g;s|initialSrvrInstance=5|initialSrvrInstance=${WEBSPEED_INITIAL_AGENTS}|g;s|minSrvrInstance=1|minSrvrInstance=${WEBSPEED_MIN_AGENTS}|g;s|maxSrvrInstance=10|maxSrvrInstance=${WEBSPEED_MAX_AGENTS}|g" /usr/dlc/properties/ubroker.properties > /usr/dlc/properties/ubroker.properties.new
 mv /usr/dlc/properties/ubroker.properties.new /usr/dlc/properties/ubroker.properties
 
+# are we using an external nameserver?
+# if [ ! -z "${NAMESERVER_HOST}" ]
+# then
+#   sed  "s|autoStart=1|autoStart=0|g;s|localhost|${NAMESERVER_HOST}|g" /usr/dlc/properties/ubroker.properties > /usr/dlc/properties/ubroker.properties.new
+#   mv /usr/dlc/properties/ubroker.properties.new /usr/dlc/properties/ubroker.properties
+# fi
+
 # first start the admin server
 echo "Starting admin server"
 proadsv -port ${ADMINSERVER_PORT} -start
 
+RETRIES=0
+while true
+do
+  if [ "${RETRIES}" -gt 10 ]
+  then
+    echo "$(date +%F_%T) ERROR: AdminServer didn't start so exiting."
+    exit 1
+  fi
+
+  if proadsv -query; then
+    break;
+  fi
+
+  sleep 1
+  RETRIES=$((RETRIES+1))
+done
+
 # let the nameserver auto start with the admin server
 # then the nameserver NS1
-#echo "Starting name server NS1"
-#nsman -port ${ADMINSERVER_PORT} -start -name NS1
+echo "Starting name server NS1"
+nsman -port ${ADMINSERVER_PORT} -start -name NS1
+
+RETRIES=0
+while true
+do
+  if [ "${RETRIES}" -gt 10 ]
+  then
+    echo "$(date +%F_%T) ERROR: NameServer didn't start so exiting."
+    exit 1
+  fi
+
+  if nsman -query -name NS1; then
+    break;
+  fi
+
+  sleep 1
+  RETRIES=$((RETRIES+1))
+done
 
 # next start webspeed broker
 echo "Starting webspeed ${WEBSPEED_SERVICE}"
